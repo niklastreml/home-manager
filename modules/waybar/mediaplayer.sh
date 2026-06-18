b567c062dadf3c00f70b3e9be27b16bef9e9cbe0
Niklas Treml <27763017+niklastreml@users.noreply.github.com>
2026-06-19 01:56:45 +0200

    fix waybar things

--- modules/waybar/mediaplayer.sh
+++ modules/waybar/mediaplayer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 player_status=$(playerctl status 2> /dev/null)
 if [ "$player_status" = "Playing" ]; then
     echo "$(playerctl metadata artist) - $(playerctl metadata title)"
