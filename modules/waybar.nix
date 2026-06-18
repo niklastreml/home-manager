0cd7805e3060724ec1060c9e146c0a7378890224
Niklas Treml <27763017+niklastreml@users.noreply.github.com>
2026-06-19 01:45:36 +0200

    waybar native
    Signed-off-by: Niklas Treml <27763017+niklastreml@users.noreply.github.com>

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -1,8 +1,6 @@
 { config, lib, pkgs, ... }:
 
 lib.mkIf config.my.isDesktop {
-  home.file.".config/waybar/config.jsonc".source = waybar/config.jsonc;
-
   home.file.".config/waybar/waybar.sh".source = waybar/waybar.sh;
   home.file.".config/waybar/waybar.sh".executable = true;
 
@@ -14,7 +12,176 @@ lib.mkIf config.my.isDesktop {
 
   programs.waybar = {
     enable = true;
-    settings = {};
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 24;
+        modules-left = [
+          "hyprland/workspaces"
+        ];
+        modules-center = [
+          "hyprland/window"
+        ];
+        modules-right = [
+          "custom/spotify"
+          "pulseaudio"
+          "group/group-hardware"
+          "network"
+          "bluetooth"
+          "battery"
+          "clock"
+          "group/group-power"
+        ];
+        "hyprland/window" = {
+          format = "{}";
+          rewrite = {
+            "(.*) - nvim" = "  [$1]";
+            "(.*) - fish" = "   [$1]";
+            "(.*) - bash" = "   [$1]";
+            "(.*) - zsh" = "   [$1]";
+          };
+          separate-outputs = true;
+        };
+        "group/group-power" = {
+          orientation = "inherit";
+          drawer = {
+            transition-duration = 250;
+            children-class = "not-power";
+            transition-left-to-right = false;
+            start-expanded = false;
+          };
+          modules = [
+            "custom/power"
+            "custom/quit"
+            "custom/lock"
+            "custom/reboot"
+          ];
+        };
+        "custom/quit" = {
+          format = "󰗼 ";
+          tooltip = false;
+          on-click = "hyprctl dispatch exit";
+        };
+        "custom/lock" = {
+          format = "󰍁 ";
+          tooltip = false;
+          on-click = "hyprlock";
+        };
+        "custom/reboot" = {
+          format = "󰜉 ";
+          tooltip = false;
+          on-click = "reboot";
+        };
+        "custom/power" = {
+          format = " ";
+          tooltip = false;
+          on-click = "shutdown now";
+        };
+        "group/group-hardware" = {
+          orientation = "inherit";
+          drawer = {
+            transition-duration = 250;
+            transition-left-to-right = false;
+            children-class = "not-hardware";
+          };
+          modules = [
+            "custom/powerprofile"
+            "cpu"
+            "memory"
+          ];
+        };
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          all-outputs = false;
+          format = "{icon}   {name}";
+          format-icons = {
+            Browser = "";
+            Terminal = "";
+            Spotify = "";
+            urgent = "";
+            default = "";
+            active = "󱓻 ";
+          };
+        };
+        clock = {
+          format-alt = "{:%d-%m-%Y %H:%M}";
+        };
+        cpu = {
+          format = "{usage}% ";
+        };
+        memory = {
+          format = "{}%  ";
+        };
+        battery = {
+          bat = "BAT0";
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          format = "{capacity}% {icon}";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
+        network = {
+          format-wifi = "{essid} ({signalStrength}%) ";
+          format-ethernet = "{ifname}: {ipaddr}/{cidr} ";
+          format-disconnected = "Disconnected ⚠";
+          on-click = "$TERMINAL -e nmtui";
+        };
+        bluetooth = {
+          format = "";
+          format-disabled = "󰂲";
+          format-connected = "";
+          tooltip-format = "Devices connected: {num_connections}";
+          on-click = "blueberry";
+        };
+        pulseaudio = {
+          format = "{volume}% {icon}";
+          format-bluetooth = "{volume}% {icon}";
+          format-muted = "";
+          format-icons = {
+            headphones = "";
+            handsfree = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            car = "";
+            default = [
+              ""
+              ""
+            ];
+          };
+          on-click = "pavucontrol";
+        };
+        "custom/powerprofile" = {
+          exec = "$HOME/.config/waybar/powerprofile.sh status";
+          max-length = 40;
+          interval = 30;
+          return-type = "json";
+          format = "{icon}";
+          on-click = "$HOME/.config/waybar/powerprofile.sh toggle";
+          format-icons = {
+            performance = " ";
+            balanced = " ";
+            power-saver = " ";
+          };
+        };
+        "custom/spotify" = {
+          format = "   {}";
+          max-length = 40;
+          interval = 30;
+          exec = "$HOME/.config/waybar/mediaplayer.sh 2> /dev/null";
+          exec-if = "pgrep spotify";
+          on-click = "hyprctl dispatch \"hl.dsp.focus({window='class:spotify'})\"";
+        };
+      };
+    };
     style = ''
     @define-color foreground #abb2bf;
     @define-color background #21252b;
