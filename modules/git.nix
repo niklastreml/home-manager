91d8eb344fc3e305ce63948d99c139088fcd787e
Niklas Treml <27763017+niklastreml@users.noreply.github.com>
2026-06-21 11:54:29 +0200

    add ignores

--- modules/git.nix
+++ modules/git.nix
@@ -1,39 +1,49 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   programs.git = {
     enable = true;
     lfs.enable = true;
     includes = [
-        {
-            condition = "gitdir:~/work/";
-            contents = {
-                user.email = "niklas.treml@telekom.de";
-            };
-        }
-        {
-            condition = "gitdir:~/code/";
-            contents = {
-                user.email = "27763017+niklastreml@users.noreply.github.com";
-            };
-        }
+      {
+        condition = "gitdir:~/work/";
+        contents = {
+          user.email = "niklas.treml@telekom.de";
+        };
+      }
+      {
+        condition = "gitdir:~/code/";
+        contents = {
+          user.email = "27763017+niklastreml@users.noreply.github.com";
+        };
+      }
     ];
     settings = {
-        init.defaultBranch = "main";
-        user = {
-            name = "Niklas Treml";
-            signingkey = "~/.ssh/id_ed25519.pub";
-        };
-        merge.tool = "nvimdiff";
-        diff = {
-            tool = "nvimdiff";
-            external = "difft";
-        };
-        commit.gpgsign = true;
-        gpg.format = "ssh";
-        rebase.updateRefs = true;
-        rerere.enabled = true;
+      init.defaultBranch = "main";
+      user = {
+        name = "Niklas Treml";
+        signingkey = "~/.ssh/id_ed25519.pub";
+      };
+      merge.tool = "nvimdiff";
+      diff = {
+        tool = "nvimdiff";
+        external = "difft";
+      };
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      rebase.updateRefs = true;
+      rerere.enabled = true;
     };
-    ignores = [ ".env" ];
+    ignores = [
+      ".env"
+      ".direnv"
+      "result"
+      "tmp"
+    ];
   };
 }
